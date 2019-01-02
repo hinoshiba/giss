1958c16fa6c512fed055f850c43a3156b610b02a
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2019-01-02 17:22:54 +0900

    updated help

--- src/giss/values/values.go
+++ src/giss/values/values.go
@@ -15,26 +15,36 @@ Credential & Repository operation
  status				Display the status of remote repository and login.
 
 Issues operation
- create 			Create a issue.
- com       <issues No>		Add a comment to the end at the specified issues.
+ create 				Create a issue.
+ com   <issue No>			Add a comment to the end at the specified issues.
 
- edit      <issues No>		Edit a title and description at the specified issues.
- close     <issues No>		Change to the close status at the specified issues.
- open      <issues No>		Change to the open status at the specified issues.
+ edit  <issue No>			Edit a title and description at the specified issues.
+ close <issue No>			Change to the close status at the specified issues.
+ open  <issue No>			Change to the open status at the specified issues.
 
- ls [ -a ] [ -l <limit cnt> ]	Display the current issues.
- 					-l <limit> : Specify the maximum display line number. By default, 20 lines.
-					-a         : Also displays closed issues. By default, only open is displayed.
- show      <issues No>		Display the specified issues detail at the iss,
- export [-a] <type>		(alpha Function) export all of the issues at stdout.
-					-a         : with export the closed issues.
-					<type>     : json, xml.
+ ls [ -a ] [ -l <limit cnt> ]		Display the current issues.
+ 						-l <limit> : Specify the maximum display line number. By default, 20 lines.
+						-a         : Also displays closed issues. By default, only open is displayed.
+ show  <issues No>			Display the specified issues detail at the issues.
 
+
+Milestone operation
+ mlls					Display the name of the milestones.
+ mlch  <issues No> <milestone name>	Change to the milestone name.
+ mldel <issues No>			Unset the milestone.
+
+Labels operation
+ lbls					Display the name of the labels.
+ lbadd <issue No> <label name>		Add to the label at selected issue.
+ lbdel <issue No> <label name>		Delete to the label at selected issue.
 
 Advance operation
-
- repo				(beta Function) A mail is automatically generated in which addresses, headers, etc. are automatically inserted.
+ repo					(beta Function) A mail is automatically generated in which addresses, headers, etc. are automatically inserted.
  				Must be set in advance in "~/.gissrc ".
+ export [-a] <type>			(test Function) export all of the issues at stdout.
+						-a         : with export the closed issues.
+						<type>     : json, xml.
+
 ---------------------------------------------------------
 
 Temporary files are managed under "~/.giss/".
